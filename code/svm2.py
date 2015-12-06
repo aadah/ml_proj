d159d96f349a4ae9e87e46593335bab5634a1247
Ahmed Ali Diriye Aden Hared <ahmed@ahmad.me>
2015-12-06 18:50:14 -0500

    removed class_weights param

--- code/svm2.py
+++ code/svm2.py
@@ -50,13 +50,13 @@ class SVM:
         self.kernel_param = kernel_param
 
         if kernel == 'linear':
-            self.model = SVC(C=C, kernel=kernel, class_weight='balanced')
+            self.model = SVC(C=C, kernel=kernel)
         elif kernel == 'poly':
             # kernel param is degree of polynomial
-            self.model = SVC(C=C, kernel=kernel, degree=kernel_param, coef0=1.0, class_weight='balanced')
+            self.model = SVC(C=C, kernel=kernel, degree=kernel_param, coef0=1.0)
         elif kernel == 'rbf':
             # kernel param is gamma param of rbf
-            self.model = SVC(C=C, kernel=kernel, gamma=kernel_param, class_weight='balanced')
+            self.model = SVC(C=C, kernel=kernel, gamma=kernel_param)
 
 
     def train(self, X, Y, alpha_thresh=1e-6,
