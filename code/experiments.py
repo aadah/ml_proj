6fd85cdb180489d00c5d6523912a396034fe74d5
Ahmed Ali Diriye Aden Hared <ahmed@ahmad.me>
2015-12-07 22:31:42 -0500

    results for varying Cs for sklearn

--- code/experiments.py
+++ code/experiments.py
@@ -193,7 +193,7 @@ def svm2_experiment(C):
         final_results[model] = results_dict
 
     print 'saving final results . . .'
-    with open('results/svm2_final_results_C_%.1f_partial_new_balance.txt' % C, 'w') as f:
+    with open('results/svm2_final_results_C_%.1f_partial_peripheral.txt' % C, 'w') as f:
         pprint(final_results, stream=f)
 
 
@@ -205,4 +205,7 @@ if __name__ == '__main__':
     #test()
     #svm2_experiment(1000000000.0) # all data
     #svm_experiment(1000000000.0) # part of data
-    svm2_experiment(1000000000.0) # part of data
+    #svm2_experiment(1000000000.0) # part of data
+    #svm2_experiment(1)
+    #svm2_experiment(10)
+    svm2_experiment(100)
